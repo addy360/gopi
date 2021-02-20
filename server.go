b707de89c2fe4fbe80f34532d3e6c0fe83d18662
Addy360 <abbyboy360@gmail.com>
2021-02-20 12:15:27 +0300

    get imp

--- server.go
+++ server.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type Post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+type postHandlers struct {
+	store map[string]Post
+}
+
+type haha string
+
 func main() {
+	postHandler := newPost()
+	http.HandleFunc("/posts", postHandler.get)
+
 	err := http.ListenAndServe(":8989", nil)
 	if err != nil {
 		log.Panic(err)
+	}
+
+}
+
+func (h *postHandlers) get(w http.ResponseWriter, r *http.Request) {
+	posts := make([]Post, len(h.store))
+	i := 0
+	for _, v := range h.store {
+		posts[i] = v
+		i++
+	}
+
+	postBs, err := json.Marshal(posts)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	w.Write(postBs)
+}
+
+func newPost() *postHandlers {
+	return &postHandlers{
+		store: map[string]Post{
+			"postone": {
+				ID:    "p1",
+				Title: "title of post one",
+				Body:  "body of post one",
+			},
+		},
 	}
 }
